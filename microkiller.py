41423aacff19a6b5ac9aaec0d7bf5eec1e4f4be0
MisterrSocial <brian.remes@gmail.com>
2026-01-02 17:08:45 -0500

    Add files via upload
    this is my first repo bear with me here

---
+++ microkiller.py
@@ -0,0 +1,50 @@
+import psutil
+import time
+import sys
+
+###################################################################
+######## add prcoess names relevant to your machine below!!########
+###################################################################
+
+TARGET_PROCESSES: list[str] = ['GameBar.exe', 'msedgewebview2.exe', 'PhoneExperienceHost.exe', 'XboxPcAppFT.exe', 'GameBarPresenceWriter.exe', 'StoreDesktopExtension.exe']
+###################################################################
+######################### l33t banner #############################
+###################################################################
+print('''
+#
+#
+#    ███    ███ ██  ██████ ██████   ██████  ██   ██ ██ ██      ██      ███████ ██████  
+#    ████  ████ ██ ██      ██   ██ ██    ██ ██  ██  ██ ██      ██      ██      ██   ██ 
+#    ██ ████ ██ ██ ██      ██████  ██    ██ █████   ██ ██      ██      █████   ██████  
+#    ██  ██  ██ ██ ██      ██   ██ ██    ██ ██  ██  ██ ██      ██      ██      ██   ██ 
+#    ██      ██ ██  ██████ ██   ██  ██████  ██   ██ ██ ███████ ███████ ███████ ██   ██ 
+#                                                                                      
+#                                                                                                                                                                                                                                                                    
+ ''')
+print(f"microkiller started // task hitlist {TARGET_PROCESSES}")
+print("press CTRL+C to stop")
+####################################################################
+CHECK_INTERVAL = 2 #### change seconds interval if you like ########
+####################################################################
+
+targets_lower: list[str] = [name.lower() for name in TARGET_PROCESSES]
+
+try:
+    while True:
+        for proc in psutil.process_iter(['pid', 'name']):
+            try:
+                proc_name = proc.info['name']
+                if proc_name and proc_name.lower() in targets_lower:
+                    print(f"gotcha!! {proc_name} (PID: {proc.info['pid']}) // killing...")
+                    proc.kill()
+                    print(f"{proc_name} is dead\n")
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                pass
+            except Exception as e:
+                print(f"err: {e}")
+
+        time.sleep(CHECK_INTERVAL)
+
+except KeyboardInterrupt:
+    print("\nkilling spree concluded...")
+    sys.exit(0)
